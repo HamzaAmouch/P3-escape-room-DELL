fcba28bef5b7d139ae54b4346e73506eedac2597
FinnBouts <9028764@student.zadkine.nl>
2026-04-01 13:03:01 +0100

    Update riddles.sql

--- sql/riddles.sql
+++ sql/riddles.sql
@@ -8,11 +8,11 @@ CREATE TABLE riddles (
 
 -- Raadsels voor het verlaten pretpark
 INSERT INTO riddles (riddle, answer, hint, roomId) VALUES
-('Ik draaide vroeger rond en rond, met muziek en vrolijkheid. Nu ben ik stil en roestig, met alleen vogels als gezelschap. Kinderen zaten ooit op mijn paardenrug. Wat ben ik?', 'Carrousel', 'Het draait in een kring, maar nu niet meer', 1),
+('Ik draaide vroeger rond en rond, met muziek en vrolijkheid. Nu ben ik stil en roestig, met alleen vogels als gezelschap. Kinderen zaten ooit op mijn paardenrug. Wat ben ik?', 'draaimolen', 'Het draait in een kring, maar nu niet meer', 1),
 
-('Ik schoot je hoog in de lucht, je hart in je keel. Nu lig ik gebroken en roestig, mijn rails naar het niets. Duizenden mensen schreeuwden van plezier op mij. Wat ben ik?', 'Achtbaan', 'Een metallisch monster met bochten en hoogte', 1),
+('Ik schoot je hoog in de lucht, je hart in je keel. Nu lig ik gebroken en roestig, mijn rails naar het niets. Duizenden mensen schreeuwden van plezier op mij. Wat ben ik?', 'achtbaan', 'Een metallisch monster met bochten en hoogte', 1),
 
-('Ik zag voetafdrukken op mijn vloer, lachende gezichten in mijn spiegels. Nu ben ik verlaten en donker, fluisteringen echo''en tegen mijn wanden. Kinderen durfden niet verder dan halverwege. Wat ben ik?', 'Spiegeldoolhof', 'Je raakt jezelf aangezicht in aangezicht, maar het is nep', 1);
+('Ik zag voetafdrukken op mijn vloer, lachende gezichten in mijn spiegels. Nu ben ik verlaten en donker, fluisteringen echo''en tegen mijn wanden. Kinderen durfden niet verder dan halverwege. Wat ben ik?', 'spookhuis', 'Je raakt jezelf aangezicht in aangezicht, maar het is nep', 1);
 
 
  InSERT INTO riddles (riddle, answer, hint, roomId) VALUES
