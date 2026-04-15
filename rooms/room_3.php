9d7022e4060170be55f98ddb52ab5bc1a8e432a2
HamzaAmouch <9028844@student.zadkine.nl>
2026-04-15 13:28:54 +0200

    update
    update

--- rooms/room_3.php
+++ rooms/room_3.php
@@ -16,6 +16,7 @@ try {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Escape Room 3</title>
+  <link rel="stylesheet" href="../css/style.css">
   <style>
     body {
       background: white;
